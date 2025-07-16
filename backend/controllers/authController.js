cfb7321a9cacd309593e4439f8cc4804b40fcccc
Stive1213 <132275722+Stive1213@users.noreply.github.com>
2025-07-16 16:13:53 +0400

    add email verification route and controller logic

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -33,4 +33,29 @@ const registerUser = async (req, res) => {
   }
 };
 
+
+const verifyEmail = async (req, res) => {
+    const {token} = req.params;
+
+    try {
+        const user = await db('users').where({verification_token: token}).first();
+
+        if (!user) {
+            return res.status(400).json({message: 'invalid or expired verification token'});
+
+        }
+
+        await db('users')
+        .where({id: user.id})
+        .update({
+            is_verified: true,
+            verification_token: null
+        });
+        res.status(200).json({message: 'Email verified successfully! You can now log in.'});
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Server error during email verification'});
+    }
+}
 module.exports = { registerUser };
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser } = require('../controllers/authController');
+const { verifyEmail } = require('../controllers/authController');
 
 router.post('/register', registerUser);
+router.get('/verify/:token', verifyEmail);
 
 module.exports = router;
