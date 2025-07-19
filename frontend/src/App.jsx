518b99bc550d30045e3d2aaea76bebf2ffe0abfc
Stive1213 <132275722+Stive1213@users.noreply.github.com>
2025-07-19 14:15:10 +0400

    feat(routes): Enhance PrivateRoute to support role-based access control

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,46 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import VerifyPage from './pages/VerifyPage';
-import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import VerifyPage from "./pages/VerifyPage";
+import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
-import ForgotPassword from './pages/ForgotPassword';
-import ResetPassword from './pages/ResetPassword';
-import AdminDashboard from './pages/AdminDashboard';
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <div className='p-4'>
+      <div className="p-4">
         <Routes>
-          <Route path="/"
-  element={
-    <PrivateRoute>
-      <Dashboard />
-    </PrivateRoute>
-  } />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/verify/:token" element={<VerifyPage />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password/:token" element={<ResetPassword />} />
-          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+          <Route
+            path="/admin-dashboard"
+            element={
+              <PrivateRoute requiredRole="admin">
+                <AdminDashboard />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,11 +1,15 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import { use } from "react";
 
-const PrivateRoute = ({ children }) => {
+const PrivateRoute = ({ children, requiredRole }) => {
   const { user } = useAuth();
 
   if (!user) {
     return <Navigate to="/login" replace />;
+  }
+  if (requiredRole && user.role !== requiredRole) {
+    return <Navigate to="/" replace />;
   }
 
   return children;
