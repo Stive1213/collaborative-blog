bf9e33d125a1d6eabf7179ca4c607bfe2519bc2a
Stive1213 <132275722+Stive1213@users.noreply.github.com>
2025-07-23 12:41:02 +0400

    feat(db): Refactor setup.js to create users and posts tables with additional fields

--- backend/db/setup.js
+++ backend/db/setup.js
@@ -1,23 +1,46 @@
-const db = require('./knex');
+const { default: knex } = require("knex");
+const db = require("./knex");
 
+db.schema.hasTable("users").then((exists) => {
+  if (!exists) {
+    return db.schema
+      .createTable("users", (table) => {
+        table.increments("id").primary();
+        table.string("name");
+        table.string("email").unique();
+        table.string("password");
+        table.boolean("is_verified").defaultTo(false);
+        table.string("google_id");
+        table.string("verification_token");
+        table.string("role").defaultTo("user");
+        table.string("reset_token").nullable();
+        table.datetime("reset_token_expiry").nullable();
+      })
+      .then(() => {
+        console.log("Users table created");
+      });
+  }
+});
 
-db.schema.hasTable('users').then(exists => {
-    if (!exists) {
-        return db.schema.createTable('users', table => {
-            table.increments('id').primary();
-            table.string('name');
-            table.string('email').unique();
-            table.string('password');
-            table.boolean('is_verified').defaultTo(false);
-            table.string('google_id');
-            table.string('verification_token');
-            table.string('role').defaultTo('user');
-            // add to setup.js or migration script
-table.string('reset_token').nullable();
-table.datetime('reset_token_expiry').nullable();
-
-        }).then(() => {
-            console.log('Users table created');
-        })
-    }
-})
+db.schema.hasTable("posts").then((exists) => {
+  if (!exists) {
+    return db.schema
+      .createTable("posts", (table) => {
+        table.increments("id").primary();
+        table
+          .integer("user_id")
+          .unsigned()
+          .references("id")
+          .inTable("users")
+          .onDelete("CASCADE");
+        table.string("title");
+        table.text("content");
+        table.string("image_path").nullable();
+        table.boolean("pin").defaultTo(false);
+        table.timestamp("created_at").defaultTo(db.fn.now());
+      })
+      .then(() => {
+        console.log("posts table created");
+      });
+  }
+});
